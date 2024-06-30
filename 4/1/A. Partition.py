a81538ef79610c810e5af41f4c2764786c8694c5
Andrey <111575820+m8driver@users.noreply.github.com>
2024-06-30 13:09:45 +0300

    Update A. Partition.py

--- 4/1/A. Partition.py
+++ 4/1/A. Partition.py
@@ -1,9 +1,9 @@
-n = int(input())\
-a = list(map(int, input().split()))\
-x = int(input())\
-ans = 0\
-for i in a:\
-    if i < x:\
-        ans += 1\
-print(ans)\
+n = int(input())
+a = list(map(int, input().split()))
+x = int(input())
+ans = 0
+for i in a:
+    if i < x:
+        ans += 1
+print(ans)
 print(n - ans)}
