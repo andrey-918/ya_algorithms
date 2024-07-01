8e1e5e95c1c348930139411c0f94438c82ca2de0
Andrey <111575820+m8driver@users.noreply.github.com>
2024-07-01 16:51:39 +0300

    Update and rename A. Дейкстра.rtf to A. Дейкстра.py

---
+++ 4/3/A. Дейкстра.py
@@ -0,0 +1,30 @@
+def get_answer(a, n, s, f):
+    answer_list = [10**10 for _ in range (n)]
+    roadList = [-1 for _ in range (n)]
+    checkList = [False for _ in range (n)]
+    i = s - 1
+    answer_list[i] = 0
+
+    while False in checkList:
+        checkList[i] = True
+        ni = i
+        for j in range (n):
+            if a[i][j] > 0 and j != i and a[i][j] + answer_list[i] < answer_list[j]:
+                answer_list[j] = a[i][j] + answer_list[i]
+                roadList[j] = i
+            if not checkList[j]:
+                ni = j
+        for j in range (n):
+            if not checkList[j] and answer_list[j] < answer_list[ni]:
+                ni = j
+        i = ni
+    if answer_list[f - 1] == 10**10:
+        return -1
+    return answer_list[f - 1]
+
+
+n, s, f = map(int, input().split())
+a = []
+for i in range (n):
+    a.append(list(map(int, input().split())))
+print(get_answer(a, n, s, f))
